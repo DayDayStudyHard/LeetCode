2a0bf703b7d7fd384115c25a2c23e868b7301b77
szn <cornerscape@gmail.com>
2022-10-06 01:32:18 +0800

    3.len of longest sub string

---
+++ LeetCode/3.LenOfLongestSubString.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode;
+
+public class MaxSubString
+{
+    public static int LenOfLongestSubString(string InStr)
+    {
+        if (string.IsNullOrEmpty(InStr)) return 0;
+        int len = InStr.Length;
+        if (len == 1) return 1;
+
+        int max = 0, left = 0;
+        Dictionary<char, int> dict = new Dictionary<char, int>(128);
+        
+        for (int right = 0; right < len; right++)
+        {
+            char ch = InStr[right];
+            left = dict.TryGetValue(ch, out var l) ? l > left ? l : left : left;
+            dict[ch] = right + 1;
+            int m = right - left + 1;
+            if (m > max) max = m;
+        }
+
+        return max;
+    }
+}
